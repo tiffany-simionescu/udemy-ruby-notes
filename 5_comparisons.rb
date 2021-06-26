a1f73b7d268cee58829788ab7206efaeac3441b5
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-26 15:17:25 -0400

    Added comparisons.

---
+++ 5_comparisons.rb
@@ -0,0 +1,10 @@
+# == - equality
+# = - asssignment
+# != - does not equal
+# > - greater than
+# >= - greater than or equal to
+# < - less than
+# <= less than or equal to
+
+puts 100 > 99 # true
+puts 100 >= 99 # true

8d8de7fff35725f63e01b4cf3ff96b2a81358f74
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-29 15:04:58 -0400

    Added authenticator

---
+++ 10_authenticator.rb
@@ -0,0 +1,38 @@
+users = [
+  { username: "tiffany", password: "1234" },
+  { username: "dougie", password: "4321" },
+  { username: "sophie", password: "1111" }
+]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record 
+    end
+  end
+  "Credentials were not correct"
+end
+
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts 
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+
+attempts = 1
+while attempts < 4 
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+
+  authentication = auth_user(username, password, users)
+  puts authentication
+
+  puts "Press q to quit or any other key to continue"
+  input = gets.chomp.downcase
+  break if input == "q"
+
+  attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 4

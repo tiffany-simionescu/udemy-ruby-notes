8a371c09f27bea7bc2a312a72190545dc1e82099
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-03 13:33:55 -0400

    Added styling.

---
+++ 11_styling.rb
@@ -0,0 +1,12 @@
+# this_is_snake_case
+# use for method names and variable assignments
+
+def my_name_is
+  # and my code is here - make sure indented 2 spaces
+end
+
+# CamelCase and camelCase = for class names
+
+class SomeClass
+  # code goes here
+end
